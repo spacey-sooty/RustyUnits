07a986d8ebc55da61b2d87b09838e744391076b4
Isaac Turner <spacey_sooty@outlook.com>
2024-02-12 13:25:23 +0800

    fix angles and add tests to them

--- src/angle.rs
+++ src/angle.rs
@@ -1,6 +1,32 @@
 use crate::Unit;
 use core::f64::consts::PI;
 
-create_unit_type!(Radians, 0.0, String::from("Angle"));
-create_unit_type!(Degrees, PI / 180.0, String::from("Angle"));
-create_unit_type!(Rotations, PI * 2.0, String::from("Angle"));
+create_unit_type!(Radians, 1.0, String::from("Angle"));
+create_unit_type!(Degrees, 180.0 / PI, String::from("Angle"));
+create_unit_type!(Rotations, 1.0 / (PI * 2.0), String::from("Angle"));
+
+#[cfg(test)]
+mod tests {
+    mod radians {
+        use crate::angle::{Unit, Radians, Rotations, Degrees};
+        use core::f64::consts::PI;
+
+        #[test]
+        fn as_si_test() {
+            let rads = Radians::new(16.5);
+            assert_eq!(rads.value, rads.get_as_si());
+        }
+
+        #[test]
+        fn rotation_conversion_test() {
+            let rads = Radians::new(PI);
+            assert_eq!(0.5, rads.convert::<Rotations>().value);
+        }
+
+        #[test]
+        fn degrees_conversion_test() {
+            let rads = Radians::new(PI);
+            assert_eq!(180.0, rads.convert::<Degrees>().value);
+        }
+    }
+}
--- src/length.rs
+++ src/length.rs
@@ -1,7 +1,10 @@
 use crate::Unit;
 
 create_unit_type!(Kilometres, 1000.0, String::from("Length"));
-create_unit_type!(Metres, 0.0, String::from("Length"));
+create_unit_type!(Metres, 1.0, String::from("Length"));
 create_unit_type!(Decimetres, 0.1, String::from("Length"));
 create_unit_type!(Centimetres, 0.01, String::from("Length"));
 create_unit_type!(Millimetres, 0.001, String::from("Length"));
+
+#[cfg(test)]
+mod tests {}
